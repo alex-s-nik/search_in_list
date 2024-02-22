6f95bbf96265bc0410826b32349960f9e0bb99aa
Alex Streltsov <to_ctrl@mail.ru>
2024-02-22 14:10:43 +0300

    feat: add filling table with fake data

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  private _itemsCount: number = faker.number.int({ min: 10, max: 30 });
+  private _data: string[] = [];
 
-  constructor() { }
+  constructor() { 
+    this.setInitialData();
+  }
+
+  setInitialData(): void {
+    for (let index = 0; index < this._itemsCount; index++) {
+      const item: string = faker.lorem.word({ length: { min: 10, max: 30 }});
+      this._data.push(item);
+    };
+  }
+
+  getData(): string[] {
+    return this._data;
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
+  public items: string[] = [];
 
+  constructor(private _searchService: SearchService) { }
+
+  ngOnInit(): void {
+      this.items = this._searchService.getData();
+  }
 }
